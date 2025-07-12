59173b8ac28418bca54d27bf52b7d1ab80152725
Gaurab Lohani <gaurablohani01@gmail.com>
2025-07-12 11:59:19 +0545

    dice_generator with image

---
+++ dice_generator.py
@@ -0,0 +1,60 @@
+import random
+dice_faces = {
+    1: (
+        "┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"
+    ),
+    2: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"
+    ),
+    3: (
+        "┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"
+    ),
+    4: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"
+    ),
+    5: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"
+    ),
+    6: (
+        "┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘"
+    )
+}
+
+def roll_dice():
+    return random.randint(1,6)
+
+input("Press Enter to roll the dice: ")
+while True:
+    dice_side= roll_dice()
+    print(f'You rolled a number {dice_side}')
+    for i in dice_faces[dice_side]:
+        print(i)
+    ch = input("Roll dice again? Press Enter to roll again or type 'n' to stop: ").lower()
+    if ch=='n':
+        break
+
+    
